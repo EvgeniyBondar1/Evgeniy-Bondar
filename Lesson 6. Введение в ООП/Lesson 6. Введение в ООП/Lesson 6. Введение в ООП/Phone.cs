d765700f8e5e8a7a17bd181e7639df79fb0a5d7a
Бондарь Евгений Игоревич <cupe7@rambler.ru>
2023-02-16 14:48:09 +0300

    Lesson 6: Домашнее задание (Задача 1)

---
+++ Lesson 6. Введение в ООП/Lesson 6. Введение в ООП/Lesson 6. Введение в ООП/Phone.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Lesson_6._Введение_в_ООП
+{
+    public class Phone
+    {
+        // Поля
+        private dynamic number;
+        private string model;
+        private string weight;
+
+        // Свойства
+        public dynamic Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public string Model 
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public string Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+
+        //Конструкторы
+        public Phone(dynamic number, string model, string weight)
+        {
+            Console.WriteLine("Пользовательский конструктор c 3-мя параметрами!");
+            this.number = number;
+            this.model = model;
+            this.weight = weight;
+        }
+        public Phone(dynamic number, string model)
+        {
+            Console.WriteLine("Пользовательский конструктор с 2-мя параметрами!");
+            this.number = number;
+        }
+        public Phone()
+        {
+            Console.WriteLine("Конструктор по умолчанию!");
+        }
+       
+        public void receiveCall(string nameCall) 
+        {
+            Console.WriteLine($"Звонит: {nameCall}");
+        }
+       
+        public string getNumber(string number)
+        {
+            return number;
+        }
+     }
+}
---
+++ Lesson 6. Введение в ООП/Lesson 6. Введение в ООП/Lesson 6. Введение в ООП/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lesson_6._Введение_в_ООП
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone(461540, "Iphone", "5c");
+            
+            Console.WriteLine($"number: {phone1.Number}, model: {phone1.Model}, weight: {phone1.Weight}");
+            phone1.receiveCall("Jeffrey Richter");
+            string result1 = phone1.getNumber(phone1.Model);
+            Console.WriteLine(result1);
+
+            Console.WriteLine(new string('-', 30));
+
+            Phone phone2 = new Phone(451234, "Samsung");
+            phone2.receiveCall("Steve McConnell");
+            Console.WriteLine($"number: {phone2.Number}, model: {phone2.Model}, weight: {phone2.Weight}");
+
+            Console.WriteLine(new string('-', 30));
+
+            Phone phone3 = new Phone();
+            phone3.receiveCall("CLR via C#");
+
+            Console.ReadKey();
+ 
+        }
+    }
+}
