a37d965ad6c0588d578d9677abdd7f3fa29b8caf
Бондарь Евгений Игоревич <cupe7@rambler.ru>
2023-02-19 18:17:28 +0300

    Lesson 7: Домашнее задание (Задача 1)

---
+++ Lesson 7. Классы в .Net/Lesson 7. Классы в .Net/Phone.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7._Классы_в_.Net
+{
+    class Phone
+    {
+        //Поля
+        private int number;
+        private string model;
+        private int weight;
+        private string name;
+
+        //Свойства
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //Конструктор по умолчанию
+        public Phone()
+        {
+
+        }
+
+        //Пользовательский конструктор
+        public Phone(int number, string model, int weight)
+            : this(355345, "Panasonic")
+        {
+            this.number = number;
+            this.model = model;
+            this.weight = weight;
+        }
+        public Phone(int number, string model)
+        {
+            this.number = number;
+            this.model = model;
+        }
+
+        //Перегрузка методов
+        public void receiveCall(string name)
+        {
+            Console.WriteLine($"Звонит: {name}!");
+        }
+        public void receiveCall(string name, int number)
+        {
+            Console.WriteLine($"Звонит: {name}, номер телефона: {number}");
+        }
+        //Возврат номера телефона
+        public int getNumber(int number)
+        {
+           return number;
+        }
+
+        //Отправка сообщений
+        public void sendMessage(int number)
+        {
+            switch (number)
+            {
+                case 452345:
+                    {
+                        processingSendMessage(452345);
+                        break;
+                    }
+                case 461540:
+                    {
+                        processingSendMessage(461540);
+                        break;
+                    }
+                case 478895:
+                    {
+                        processingSendMessage(478895);
+                        break;
+                    }
+                default:
+                    break;
+            }
+        }
+
+        //Обработка отправки сообщений (инкапсуляция)
+        private void processingSendMessage(int number)
+        {
+            Console.WriteLine($"На номер: {number}, будет отправлено сообщение!\n");
+        }
+    }
+}
---
+++ Lesson 7. Классы в .Net/Lesson 7. Классы в .Net/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Lesson_7._Классы_в_.Net
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задача 1. Класс Phone
+            Phone phone1 = new Phone(452345, "Iphone 5", 100);
+            Phone phone2 = new Phone(461540, "Iphone 6", 200);
+            Phone phone3 = new Phone(478895, "Iphone 7", 300);
+           
+            phone1.receiveCall("Роберт Мартин");
+            phone1.receiveCall("Роберт Мартин", 452345);
+            phone1.sendMessage(phone1.Number);
+            int getNumber1 = phone1.getNumber(452345);
+           
+            phone1.Number = 452345;
+            phone1.Model = "Iphone 5";
+            phone1.Weight = 100;
+           
+            phone2.receiveCall("Троелсен Эндрю, Джепикс Филипп");
+            phone2.receiveCall("Троелсен Эндрю, Джепикс Филипп", 461540);
+            phone2.sendMessage(phone2.Number);
+            int getNumber2 = phone2.getNumber(461540);
+            
+            phone2.Number = 461540;
+            phone2.Model = "Iphone 6";
+            phone2.Weight = 200;
+
+            phone3.receiveCall("Рихтер Джеффри");
+            phone3.receiveCall("Рихтер Джеффри", 478895);
+            phone3.sendMessage(phone3.Number);
+            int getNumber3 = phone3.getNumber(478895);
+            
+            phone3.Number = 478895;
+            phone3.Model = "Iphone 7";
+            phone3.Weight = 300;
+
+            Console.WriteLine($"Вывод номера телефона: {getNumber1}");
+            Console.WriteLine($"Вывод номера телефона: {getNumber2}");
+            Console.WriteLine($"Вывод номера телефона: {getNumber3}\n");
+            Console.WriteLine($"Номер: {phone1.Number}, модель: {phone1.Model}, вес: {phone1.Weight}");
+            Console.WriteLine($"Номер: {phone2.Number}, модель: {phone2.Model}, вес: {phone2.Weight}");
+            Console.WriteLine($"Номер: {phone3.Number}, модель: {phone3.Model}, вес: {phone3.Weight}");
+
+            Console.ReadKey();
+            #endregion
+        }
+    }
+}
