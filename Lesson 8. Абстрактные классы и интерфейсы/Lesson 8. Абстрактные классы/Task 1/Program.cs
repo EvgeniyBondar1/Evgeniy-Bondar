ebb465bd4ba3b31b1ecf2368d6ebd6bcf28134bd
Бондарь Евгений Игоревич <cupe7@rambler.ru>
2023-02-24 15:26:26 +0300

    Lesson 8: Домашнее задание (Задача 1)

---
+++ Lesson 8. Абстрактные классы и интерфейсы/Lesson 8. Абстрактные классы/Task 1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        /// <summary>
+        /// Задача 1. Вывод списка дней недели через перечисление в C#.
+        /// </summary>
+        enum DaysWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sanday
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DaysWeek.Monday);
+            Console.WriteLine(DaysWeek.Tuesday);
+            Console.WriteLine(DaysWeek.Wednesday);
+            Console.WriteLine(DaysWeek.Thursday);
+            Console.WriteLine(DaysWeek.Friday);
+            Console.WriteLine(DaysWeek.Saturday);
+            Console.WriteLine(DaysWeek.Sanday);
+
+            Console.ReadKey();
+        }
+    }
+}
